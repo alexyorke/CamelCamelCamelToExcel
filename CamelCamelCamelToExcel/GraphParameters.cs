80de4a0eaa6fba5d4b896a7b61e64fe19a1e49e6
Alex Yorke <alexyorke@users.noreply.github.com>
2020-08-14 21:30:21 -0400

    Remove unused methods

--- CamelCamelCamelToExcel/GraphParameters.cs
+++ CamelCamelCamelToExcel/GraphParameters.cs
@@ -9,28 +9,12 @@ namespace CamelCamelCamelToExcel
         public readonly uint Width;
         public readonly uint Height;
 
-        public GraphParameters()
-        {
-            StartDate = DateTime.Now - TimeSpan.FromDays(365);
-            EndDate = DateTime.Now;
-            Width = 1000;
-            Height = 1000;
-        }
-
         public GraphParameters(DateTime startDate, DateTime endDate, uint width, uint height)
         {
             StartDate = startDate;
             EndDate = endDate;
             Width = width;
             Height = height;
-        }
-
-        public GraphParameters(DateTime startDate)
-        {
-            StartDate = startDate;
-            EndDate = DateTime.Now;
-            Width = 1000;
-            Height = 1000;
         }
     }
 }
