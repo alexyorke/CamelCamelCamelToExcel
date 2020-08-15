7c77b72f9258dae1656b7130544b17164d0285da
Alex Yorke <alexyorke@users.noreply.github.com>
2020-08-14 21:28:50 -0400

    Fix naming

--- CamelCamelCamelToExcel/ProductPageBuilder.cs
+++ CamelCamelCamelToExcel/ProductPageBuilder.cs
@@ -19,20 +19,20 @@ namespace CamelCamelCamelToExcel
             var web = new HtmlWeb();
             var doc = web.Load(_url);
 
-            var MaxPrice = Convert.ToDecimal(doc.DocumentNode.SelectNodes("//div[1]/div[@class=' margined' and 1]/div[@class='row column' and 2]/div[@class='row' and 1]/div[1]/table[@class='product_pane' and 1]/tbody[1]/tr[@class='highest_price' and 2]/td[2]").Nodes().FirstOrDefault()?.InnerText?.Replace("$", ""));
-            var MinPrice = Convert.ToDecimal(doc.DocumentNode.SelectNodes("//div[1]/div[@class=' margined' and 1]/div[@class='row column' and 2]/div[@class='row' and 1]/div[1]/table[@class='product_pane' and 1]/tbody[1]/tr[@class='lowest_price' and 3]/td[2]").Nodes().FirstOrDefault()?.InnerText?.Replace("$", ""));
+            var maxPrice = Convert.ToDecimal(doc.DocumentNode.SelectNodes("//div[1]/div[@class=' margined' and 1]/div[@class='row column' and 2]/div[@class='row' and 1]/div[1]/table[@class='product_pane' and 1]/tbody[1]/tr[@class='highest_price' and 2]/td[2]").Nodes().FirstOrDefault()?.InnerText?.Replace("$", ""));
+            var minPrice = Convert.ToDecimal(doc.DocumentNode.SelectNodes("//div[1]/div[@class=' margined' and 1]/div[@class='row column' and 2]/div[@class='row' and 1]/div[1]/table[@class='product_pane' and 1]/tbody[1]/tr[@class='lowest_price' and 3]/td[2]").Nodes().FirstOrDefault()?.InnerText?.Replace("$", ""));
 
-            var StartDateRaw = string.Join("", doc.DocumentNode
+            var startDateRaw = string.Join("", doc.DocumentNode
                 .SelectNodes(
                     "//div[1]/div[@class=' margined' and 1]/div[@class='row column' and 2]/div[@class='row' and 1]/div[1]/p[@class='grey' and 1]/small[1]")
                 .Nodes().Select(x => x.InnerText).ToList());
 
-            var StartDate = DateTime.Parse(Regex.Match(StartDateRaw, @"(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec) \d+\, \d+").Value);
+            var startDate = DateTime.Parse(Regex.Match(startDateRaw, @"(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec) \d+\, \d+").Value);
 
-            var graphParameters = new GraphParameters(StartDate, DateTime.Now, 1000, 1000);
-            var graph = new Graph(_url, graphParameters, MinPrice, MaxPrice);
+            var graphParameters = new GraphParameters(startDate, DateTime.Now, 1000, 1000);
+            var graph = new Graph(_url, graphParameters, minPrice, maxPrice);
 
-            return new ProductPage {Url = _url, Graph = graph, MinPrice = MinPrice, MaxPrice = MaxPrice };
+            return new ProductPage {Url = _url, Graph = graph, MinPrice = minPrice, MaxPrice = maxPrice };
         }
     }
 }
